351a859a7a8bb3bf3943a7e6c98cb0886e12ff38
GrzegorzDuzynski <grzegorz.duzynski@gmail.com>
2023-12-26 16:36:23 +0100

    next rew

--- Desktop/react/insurance-landing-page/src/components/Reputation.js
+++ Desktop/react/insurance-landing-page/src/components/Reputation.js
@@ -58,13 +58,13 @@ const Reputation = () => {
   useEffect(() => {
     let config = {
       method: "get",
-      url: "/details/json?place_id=ChIJexDWKjAnGEcRl4EbYC5gvRI&key=AIzaSyAHQzVbDSLvoAP4wJgRQpm7y8XXY1KUV9E",
+      url: "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJexDWKjAnGEcRl4EbYC5gvRI&key=AIzaSyAHQzVbDSLvoAP4wJgRQpm7y8XXY1KUV9E",
       secure: false,
     };
     const fetchData = async () => {
       const result = await axios(config).then(function (response) {
         setOpinionsObj(response.data.result.reviews);
-        // console.log(response.data.result.reviews);
+        console.log(response.data.result.reviews, config);
         // setOpinionsObj(response.data);
       });
     };
